7258aff66a54670f81765ad65f803f1ea728edac
suXin <suxinjke@gmail.com>
2026-01-26 18:37:23 +0700

    refactor: remove packages/tsconfig, skip type check when building packages
    The initial reason for common config was due to monorepo recipes usually having it involved.
    Rather than having it confusing on which config extends which, treat the packages in isolation.
    Along the way, cut unnecessary compiler options.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 import * as url from 'url'
 import * as path from 'path'
 const thisModuleDir = path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     coverage: {
       provider: 'istanbul',
